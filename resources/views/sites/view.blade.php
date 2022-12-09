58c5c852e735e067345270d267f55c0beb7ced4d
dev <admin@idevgames.co.uk>
2022-12-09 23:08:55 +0000

    Fix versioning on JS

--- resources/views/sites/view.blade.php
+++ resources/views/sites/view.blade.php
@@ -10,17 +10,17 @@
         <title>Web Analytics for {!! $domain !!} - {{ config('app.name', 'Laravel') }}</title>
 
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="{{ asset('js/analytical.js') }}" data-debug-analytical-js="true" defer></script>
+        <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="{{ mix('js/analytical.js') }}" data-debug-analytical-js="true" defer></script>
 
         <!-- Styles -->
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
         <!-- Favicon -->
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
-        <link rel="manifest" href="/favicon/site.webmanifest">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
 
     </head>
     <body>
