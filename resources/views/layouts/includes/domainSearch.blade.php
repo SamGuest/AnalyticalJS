981911118e80457e3f25836be882e0586373bf64
dev <admin@idevgames.co.uk>
2022-12-09 14:00:50 +0000

    Changed the home page

---
+++ resources/views/layouts/includes/domainSearch.blade.php
@@ -0,0 +1,28 @@
+<div class="domainPanel">
+                
+    <div class="domainPanelInner">
+    
+        <form action="#" method="GET">
+
+            @csrf <!-- {{ csrf_field() }} -->
+
+            <div class="row">
+
+                <div class="col-8">
+            
+                    <input type="text" name="website" id="website" class="input" value="">
+
+                </div>
+                <div class="col-4">
+
+                    <input type="submit" class="btn btn-gradient-purple-alternative" value="View Stats">
+
+                </div>
+            
+            </div>
+
+        </form>
+    
+    </div>
+
+</div>
