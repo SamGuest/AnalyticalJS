f2c0a96011be0ac976b9da602419405b258768fb
Sam Guest <samguest378@gmail.com>
2022-12-15 17:48:26 -0500

    Updates to the auth system and admin dashboard

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,17 +1,48 @@
 @include("layouts.header")
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<div class="bar-holder">
 
-                <div class="card-body">
+    <div class="bar fill1"></div>
+
+    <div class="bar fill2"></div>
+
+    <div class="bar fill3"></div>
+
+    <div class="bar fill4"></div>
+
+    <div class="bar fill1"></div>
+
+    <div class="bar fill5"></div>
+
+    <div class="bar fill6"></div>
+
+    <div class="bar fill1"></div>
+
+    <div class="bar fill2"></div>
+
+    <div class="bar fill3"></div>
+
+</div>
+
+<div class="hero-head">
+
+    <div class="hero-content">
+
+        <h1 class="logo">Register</h1>
+
+        <div class="spacer20"></div>
+
+        <div class="container panel">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <div class="spacer20"></div>
+
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                        <div class="row">
+                            <label for="name" class="col-md-4 text-right login-page-text">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -24,11 +55,13 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                        <div class="spacer20"></div>
 
+                        <div class="row">
+                            <label for="email" class="col-md-4 text-right login-page-text">{{ __('Email Address') }}</label>
+        
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -38,11 +71,13 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                        <div class="spacer20"></div>
+
+                        <div class="row">
+                            <label for="password" class="col-md-4 text-right login-page-text">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -52,26 +87,58 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+                        <div class="spacer20"></div>
+
+                        <div class="row">
+                            <label for="password-confirm" class="col-md-4 text-right login-page-text">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="spacer20"></div>
+
+                        <div class="row">
+                            <div class="col-md-4 text-right"></div>
+
+                            <div class="col-md-3 text-left">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                <label class="login-page-text remember-me" for="remember">
+                                    {{ __('Remember Me') }}
+                                </label>
+                            </div>
+
+                            <div class="col-md-3 text-right">
+                                @if (Route::has('password.request'))
+                                <a class="login-page-text forgot-password" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                            @endif
+                            </div>
+                        </div>
+
+                        <div class="spacer20"></div>
+
+                        <div class="rows">
+                            <div class="col-md-3 text-center">
+                                <button type="submit" class="btn btn-gradient-purple-alternative btn100">
                                     {{ __('Register') }}
                                 </button>
                             </div>
                         </div>
                     </form>
+
+                    <div class="spacer20"></div>
+
                 </div>
             </div>
         </div>
-    </div>
-</div>
 
+        <div class="spacer20"></div>
+
+    </div>
+
+</div>
 @include("layouts.footer")
