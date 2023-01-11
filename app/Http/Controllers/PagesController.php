02873a884162770e99715ca1ac51d5198157aa7b
dev <admin@idevgames.co.uk>
2023-01-11 17:10:10 +0000

    details page edit

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 
 
@@ -10,6 +11,13 @@ class PagesController extends Controller
 {
     public function details()
     {
-        return view('user.details');
+        if (Auth::check()) {
+            $username = Auth::user()->name;
+            $email = Auth::user()->email;
+            return view('user.details')->with('username', $username)->with('email', $email);
+        }
+        else{
+            return redirect()->route('home');
+        }
     }
 }
--- resources/views/user/details.blade.php
+++ resources/views/user/details.blade.php
@@ -24,18 +24,58 @@
 
 </div>
 
-<div class="container panel">
-    <div class="row">
-        <div class="col-md-12">
+<div class="hero-head">
 
-            <div class="spacer20"></div>
+    <div class="hero-content">
+    
+        <h1 class="logo">My Details</h1>
+    
+        <div class="spacer20"></div>
 
-            {{ Auth::user()->name }}
-            {{ Auth::user()->email }}
+            <div class="container panel">
 
-            <div class="spacer20"></div>  
+                <div class="row">
+                    
+                    <div class="col-md-12">
+
+                        <div class="spacer20"></div>
+
+                            <div class="col-md-6">
+
+                                {{ $username }}
+
+                            </div>
+
+                        <div class="spacer20"></div>  
+
+                    </div>
+
+                </div>
+
+                <div class="row">
+                    
+                    <div class="col-md-12">
+
+                        <div class="spacer20"></div>
+
+                            <div class="col-md-6">
+
+                                {{ $email }}
+
+                            </div>
+
+                        <div class="spacer20"></div>  
+
+                    </div>
+
+                </div>
+
+            </div>
 
         </div>
+
     </div>
+
 </div>
+
 @include("layouts.footer")
